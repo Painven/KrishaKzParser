1ad9ff14a3add6e97fa8ef75a76efa29d9bafcc4
Дмитрий Смирнов <painven@gmail.com>
2022-12-31 02:06:40 +0300

    Add project files.

---
+++ KrishaKzDesktop/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using KrishaKzDesktop.ViewModels;
+using System.Windows;
+
+namespace KrishaKzDesktop;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var vm = new MainWindowViewModel();
+        var mainWindow = new MainWindow();
+        mainWindow.DataContext = vm;
+        mainWindow.Show();
+    }
+}
